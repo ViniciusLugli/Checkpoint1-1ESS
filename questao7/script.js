733715ba943f3537f256a2fcc6d346d984a38811
matheusvictorio <matheusvic2016@gmail.com>
2025-04-01 20:31:02 -0300

    Questão 7

---
+++ questao7/script.js
@@ -0,0 +1,13 @@
+let a = true;
+let b = false;
+let x = 10;
+let y = 5;
+
+console.log("true && false:", a && b); 
+console.log("10 > 5 && 5 > 4:", x > y && y > 4); 
+
+console.log("true || false:", a || b); 
+console.log("10 < 5 || 10 > 5:", x < y || x > y);
+
+console.log("!true:", !a); 
+console.log("!false:", !b);
